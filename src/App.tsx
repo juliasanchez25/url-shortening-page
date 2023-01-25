b22e15245eb2409cc15a99c2433b4a77c1fb03d3
Julia <2509juliasanchez@gmail.com>
2023-01-25 11:55:20 -0300

    feat: configuring files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-        
+      <h1>More than just shorter links</h1>
     </div>
   )
 }
