f73f84d0020ce0ddb585905a2f66509d8266e819
Julia <2509juliasanchez@gmail.com>
2023-01-30 09:20:05 -0300

    fix: white box align

--- src/components/UrlShorteningComponent.tsx
+++ src/components/UrlShorteningComponent.tsx
@@ -9,14 +9,6 @@ import "../styles/UrlShorteningComponent.scss";
 const UrlShortening = ({ loadUrl }: UrlProps) => {
   const [originalUrl, setOriginalUrl] = useState("");
 
-  const [inputValue, setInputValue] = useState('');
-
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    console.log('Valor enviado:', inputValue);
-    setInputValue('');
-  }
-
   const handleKeyDown = (e: KeyboardEvent) => {
     if (e.key === "Enter") {
       loadUrl(originalUrl);
@@ -28,11 +20,9 @@ const UrlShortening = ({ loadUrl }: UrlProps) => {
       <div className="short-url-container__item">
         <input
           type="text"
-          value={inputValue}
           placeholder="Shorter a link here..."
-          onChange={e => [setOriginalUrl(e.target.value),,setInputValue(e.target.value)]} 
+          onChange={e => setOriginalUrl(e.target.value)} 
           onKeyDown={handleKeyDown}
-          onSubmit={handleSubmit}
         ></input>
         <button
           onClick={() => loadUrl(originalUrl)}
